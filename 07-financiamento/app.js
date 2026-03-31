24b319989884230702250f568615fc9a3d4d7f3f
ISRAEL FUJIMOTO <25203688@CFP127ALUNO.LOCAL>
2026-03-31 17:01:02 -0300

    Atividades quase finalizadas

---
+++ 07-financiamento/app.js
@@ -0,0 +1,16 @@
+'use strict'
+
+function criarListaMes(meses){
+
+    let listaMes = []
+
+    for (let i = 1; i <= meses ; i++) {
+
+        listaMes.push(i)
+    }
+
+    return listaMes
+}
+
+
+
